262b84b0449d12dafe9c72ae25a004c727da061a
Sebastian V <sebastian.vera.uy@gmail.com>
2025-08-31 20:53:43 -0300

    Terminé pero con errores, las clases soy muy claras pero me falta práctica :/

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from inicio.views import inicio
+from inicio.views import inicio, crear_moto, listado_moto
 
 urlpatterns = [
-    path('', inicio, name='inicio')
+    path('', inicio, name='inicio'),
+    path('moto/crear/', crear_moto, name='crear_moto'),
+    path('moto/', listado_moto, name='listado_moto'),
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from inicio.models import moto
+from inicio.forms import FormularioCreacionMoto
+
 
 def inicio(request):
    return render(request,'inicio.html' )
+
+def crear_moto(request):
     
+    if request.method == "POST":
+        formulario = FormularioCreacionMoto(request.POST)
+        if formulario.is_valid():
+            marca_nueva = formulario.cleaned_data.get('marca')
+            modelo_nuevo = formulario.cleaned_data.get('modelo')
+            
+            moto = moto(marca= marca_nueva, modelo = modelo_nuevo)
+            moto.save()
+    
+    else:
+        formulario= FormularioCreacionMoto()
+
+    return render(request, 'inicio/crear_moto.html', {'formulario': formulario })
+
+def listado_moto(request):
+    
+    moto = moto.objects.all
+    
+    return render(request, 'inicio'/listado_moto.html, {'listado_de_moto': moto})
+
     #return HttpResponse('<h1>HOLA ESTA ES MI PRIMERA VISTA</h1>')
 
-# Create your views here.
+# Create your views here
